1e13c8e10316d8e382169d1c817e33daed6965ec
Mateusz Wojdalski <wojdalski.m@gmail.com>
2022-05-27 20:18:24 +0200

    create nav buttons based on data from api

--- script.js
+++ script.js
@@ -1,13 +1,26 @@
 window.onload = init;
 
 const BASE_URL = "https://swapi.dev/api/";
+let urls = [];
+let endpoints = [];
+let buttons = [];
 
-async function getApi(){
+async function init(){
     const response = await fetch(BASE_URL);
     const data = await response.json();
-    console.log(data)
+    endpoints = Object.keys(data);
+    urls = Object.values(data);
+
+    createButtons();
 }
 
-function init(){
-    getApi();
+function createButtons() {
+  endpoints.forEach((endpoint) => {
+    const element = document.createElement("button");
+    document.getElementById("nav-buttons").appendChild(element);
+    element.innerHTML = endpoint[0].toUpperCase() + endpoint.slice(1);
+    element.className = "button";
+    element.id = `${endpoint}Btn-js`;
+    buttons.push(element);
+  });
 }
